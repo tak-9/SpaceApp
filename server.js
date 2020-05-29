d38f78ce252a32daa839a40de661399fa7d1b136
Takuji Okubo <takuji.okubo@gmail.com>
2020-05-30 01:08:41 +1000

    Fixed CORS issue

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(
     cors({
-      origin: "http://localhost:3000", // allow to server to accept request from different origin
+      origin: ["http://localhost:3000", "https://isolution.herokuapp.com"], // allow to server to accept request from different origin
       methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
       credentials: true // allow session cookie from browser to pass through
     })
