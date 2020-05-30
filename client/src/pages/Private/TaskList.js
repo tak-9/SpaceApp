81c6bbb51fb62ed17feb18c06ec296125d31bd55
Lisa Ostman <peolina@live.com.au>
2020-05-30 20:11:15 +1000

    task list now finds and gathers all the tasks in the table

--- client/src/pages/Private/TaskList.js
+++ client/src/pages/Private/TaskList.js
@@ -1,34 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import './TaskList.css';
 import { serverUrl } from '../../utils/env';
 import axios from 'axios';
 import TaskCard from "./TaskCard";
 
 function TaskList(props) {
-    var tasks = {}
+    const [tasks, setTasks] = useState('');
 
     useEffect(() => {
-        var url = serverUrl + '/api/get';
+        var url = serverUrl + '/api/all';
+
         axios.get(url)
-        .then((res)=>{
+        .then(res => {
+            console.log("--------------------")
             console.log("### OK TaskList useEffect() axios get ", url, res);
-            tasks = res;
-            console.log(res);
+            setTasks(res);
         })
-        .catch((err)=>{
+        .catch(err => {
             console.log("### Error TaskList useEffect() axios get ", err);
         });
     },[])
 
     return (
         <div className="tasklist">
-            {/* {tasks.map(task => (
+            {tasks.map(task => (
                                 <TaskCard
                                     taskname={task.taskname}
                                     category={task.category}
                                     completed={task.completed}
                                 />
-                            ))} */}
+                            ))} 
         </div>
     );
 }
